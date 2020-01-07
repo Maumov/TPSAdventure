b9ae839cf3cb24b9788763822a6d88e558052a33
Insidios <mau.movilla@gmail.com>
2020-01-07 15:34:02 -0500

    Starting the WFC

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int viewSize;
+    [Header ("Sample")]
+    public List<Part> PartsLibrary;
+
+    [Header("Generation")]
+    public int sizeX;
+    public int sizeY;
+    public int sizeZ;
+    public GameObject SlotPrefab;
+    public List<Part> AllSlotsCreated;
+    public int randomSeed;
+    public void Start() {
+        AllSlotsCreated = new List<Part>();
+        StartCoroutine(GenerateSlots());
+       
+    }
+
+    IEnumerator GenerateSlots() {
+        for(int i = 0; i < sizeX; i++) {
+            for(int j = 0; j < sizeY; j++) {
+                for(int k = 0; k < sizeZ; k++) {
+                    Part p = new Part();
+                    AllSlotsCreated.Add(p);
+                    yield return null;
+                }
+            }
+        }
+        StartCoroutine(TheStart());
+    }
+
+    IEnumerator TheStart() {
+        Random.InitState(randomSeed);
+        
+        yield return null;
+
+
+    }
+}
+
+[System.Serializable]
+public class Part
+{
+    public string Name;
+    public int partNumber;
+    //What can be connected with me;
+    public int connectionLeft;
+    public int connectionRight;
+    public int connectionFront;
+    public int connectionBack;
+
+    float entropy;
+
+    public void CalculateEntropy() {
+        
+    }
+}
+
+
---
+++ Assets/Slot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Slot : MonoBehaviour
+{
+    public bool Filled;
+    public int myValue = -1;
+
+    public Slot Top, Bottom, Left, Right, Front, Back;
+
+    public ProbabilityToBe myProbability;
+
+    public List<GameObject> Visuals;
+    private void Start() {
+        foreach(GameObject g in Visuals) {
+            g.SetActive(false);
+        }
+    }
+    public void MyValueIsSet() {
+        if(myValue != -1) {
+            Visuals[myValue].SetActive(true);
+        }
+    }
+
+}
+
+[System.Serializable]
+public class ProbabilityToBe
+{
+    public List<int> value;
+    public List<float> probability;
+}
